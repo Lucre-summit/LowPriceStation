9066e93858f02d5902cd1149fc300fc2e3204b8a
koontathai <koontathai@gmail.com>
2026-09-15 18:10:57 +0700

    chore: drop the importer's exploration scratch files

--- data/_explore6.py
+++
@@ -1,7 +0,0 @@
-import json, re, collections
-
-d = json.load(open('data/raw/bma_arcgis_dcevbmA_chargingstation.json', encoding='utf-8'))
-fs = [f['attributes'] for f in d['features']]
-desc = collections.Counter((a['PROVIDER'], a['DESCRIPTION']) for a in fs)
-for (p, ds), v in sorted(desc.items(), key=lambda kv: (kv[0][0], -kv[1])):
-    print(v, '|', p, '|', repr(ds))
